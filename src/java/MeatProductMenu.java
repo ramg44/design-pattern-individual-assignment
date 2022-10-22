1703bd35b9aa6eef78577ffb4f39130bd5c1a6c4
ramg44 <112654778+ramg44@users.noreply.github.com>
2022-10-21 19:51:04 -0700

    Update MeatProductMenu.java

--- src/java/MeatProductMenu.java
+++ src/java/MeatProductMenu.java
@@ -1,29 +1,95 @@
-package java;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MeatProductMenu implements ProductMenu {
+    List<Product> productList;
 
-	public void showMenu() {
+    public static String readFileAsString(String fileName) throws Exception {
+        String data = "";
+        data = new String(Files.readAllBytes(Paths.get(fileName)));
+        return data;
+    }
 
-	}
 
-	public void showAddButton() {
+    public MeatProductMenu() throws Exception {
+        productList = new ArrayList<>();
+        String path = System.getProperty("user.dir");
+        String productInfos[] = readFileAsString(path.concat("/src/DataBase/ProductInfo.txt")).split("\\R");
+        for (String productInfo : productInfos) {
 
-	}
+            String product[] = productInfo.split(":");
 
-	public void showViewButton() {
+            if (product[0].equals("Meat")) {
+                productList.add(new Product(product[0], product[1]));
+            }
+        }
+    }
 
-	}
+    public List<Product> getProductList() {
+        return productList;
+    }
 
-	public void showRadioButton() {
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
 
-	}
+    @Override
+    public void ShowMenu() {
 
-	public void showLabels() {
+        System.out.println("Meat Product Menu");
+        String leftAlignFormat = "| %-15s | %-15s |%n";
 
-	}
+        System.out.format("+-----------------+------+%n");
+        System.out.format("| Product Category   | Product name |%n");
+        System.out.format("+-----------------+------+%n");
+        for (Product product : productList) {
+            System.out.format(leftAlignFormat, product.getProductCategory(), product.getProductName());
+        }
 
-	public void showComBoxes() {
+        System.out.format("+-----------------+------+%n");
 
-	}
+    }
 
+    @Override
+    public void showAddButton() {
+
+    }
+
+    @Override
+    public void showViewButton() {
+
+    }
+
+    @Override
+    public void showRadioButton() {
+
+    }
+
+    @Override
+    public void showLabels() {
+
+    }
+
+    @Override
+    public void showComboxes() {
+
+    }
+
+    @Override
+    public void add(Product product) {
+        productList.add(product);
+    }
+
+    @Override
+    public ListIterator getIterator() {
+        return null;
+    }
+    //   Visitor Design pattern is used here
+    @Override
+    public void accept(NodeVisitor nodeVisitor) throws Exception {
+        nodeVisitor.visit(this);
+
+    }
 }
